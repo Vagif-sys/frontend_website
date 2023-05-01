2471d00d1d2a8e54dc73c1f61bfd2f96163bc8ce
Vagif <z.b.2021@list.ru>
2023-05-01 21:08:54 +0400

    'BakuDevs'

---
+++ js/javascript.js
@@ -0,0 +1,24 @@
+function openNav(){
+
+    document.getElementById("resMenu1").style.display = "block";
+    document.getElementById("resMenu1").style.width = "250px";
+}
+
+function closeNav(){
+
+    document.getElementById("resMenu1").style.width = "0";
+    document.getElementById("resMenu1").style.display = "none";
+}
+
+
+function deskOpenNav(){
+
+    document.getElementById("deskNav").style.display = "flex";
+   
+}
+
+function deskcloseNav(){
+
+    document.getElementById("deskNav").style.display = "none";
+   
+}
